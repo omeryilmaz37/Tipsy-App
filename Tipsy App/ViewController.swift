dedf003c60fdfc4a2ba23ab97d866bab5e172d1f
Ömer Yılmaz <omerryilmazz.37@gmail.com>
2024-07-02 17:26:18 +0300

    Initial Commit

---
+++ Tipsy App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy App
+//
+//  Created by Ömer Yılmaz on 2.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
